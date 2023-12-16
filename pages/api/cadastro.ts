7ebd9b31b018b714cc5815111eb26ac660929afb
Bruno Matheus Stefano Leite <105085425+BrunoMatheus96@users.noreply.github.com>
2023-12-16 00:56:21 -0300

    finalização do upload das imagens

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,42 +1,81 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
-import type { CadastroRequisicao } from '../../types/CadastroRequisicao';
-import { UsuarioModel } from '../../models/UsuarioModel';
-import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
-import md5 from 'md5';
+import type { NextApiRequest, NextApiResponse } from "next"; // Request, Response e Handler padrão do Next
+import nc from "next-connect"; // Importando o Next Connect para facilitar a conexão HTTP
+import md5 from "md5"; // Importando uma biblioteca de cripitografia simples para não manter a senha do usuário sem criptografia
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg"; // Importando o tipo de resposta padrão que criamos
+import type { CadastroRequisicao } from "../../types/CadastroRequisicao"; // Importando o tipo de requisição de cadastro padrão que criamos
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB"; // Importando o middleware de conexão com DB que foi criado
+//import { politicaCORS } from "@/middlewares/politicaCORS"; // Importando o middleware de CORS que criamos
+import { UsuarioModel } from "../../models/UsuarioModel"; // Importando o model do Usuário
+import { upload, uploadImagemCosmic } from "../../service/uploadImagemCosmic"; // Importando as funções que criamos para utilização do Cosmic e Mutler
 
-const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+const handler = nc() // Utilizando o nc do Next Connect que serve para "gerenciar" as requisições e resposta HTTP
+    .use(upload.single("file")) // utiliza o Multer para fazer upload de um único arquivo
+    .post(
+        // Utiliza o método POST do NC
+        async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+            try {
+                // Trativa de erro, caso dê algum erro na tentativa o catch pegará esse erro e será possível tratar ele
+                const usuario = req.body as CadastroRequisicao; // Criação do corpo da Request conforme o tipo padrão que criamos
 
-    if (req.method === 'POST') {
-            const usuario = req.body as CadastroRequisicao; /*A const "usuario" vai receber o payload da requisição e vai preencher esse payload com as variáveis de CadastroRequisição.ts*/
+                // Verificações de Nome, Email e Senha. Não é necessário a de imagem pois não é obrigatório
 
-        //Se não informar o nome ou so uma letra
-        if (!usuario.nome || usuario.nome.length < 2) {
-            return res.status(400).json({ erro: 'Nome invalido. Tente novamente.' }); //Vai retornar se colocarem alguma informação de login e senha errada
+                if (!usuario.nome || usuario.nome.length < 2) {
+                    // Verifica se há nome e se ele tem um tamanho maior que 2, caso não atenda retorna erro de Nome inválido
+                    return res.status(400).json({ erro: "Nome inválido" });
+                }
+
+                if (
+                    // Verifica se o e-mail tem uma estrutura simples de email:
+                    // -Verifica se existe
+                    // -Verifica se tem mais que 5 caracteres
+                    // -Verifica se possui o @
+                    // -Verifica se tem um .
+                    !usuario.email ||
+                    usuario.email.length < 5 ||
+                    !usuario.email.includes("@") ||
+                    !usuario.email.includes(".")
+                ) {
+                    return res.status(400).json({ erro: "Email inválido" }); // Retorna Email inválido caso qualquer uma das condições não sejam atendidas
+                }
+                if (!usuario.senha || usuario.senha.length < 4) {
+                    // Verifica o tamanho da senha caso não seja maior que 4 retorna um erro
+                    return res.status(400).json({ erro: "Senha inválida" });
+                }
+
+                // Verificando se já existe usuário com o mesmo email que foi informado no cadastro
+
+                const usuarioComMesmoEmail = await UsuarioModel.find({
+                    // Método de pesquisa em um Model, é passado os parâmetros e é retornado um objeto que contenha o que foi passado
+                    email: usuario.email, // Passando um JSON com o usuário do cadastro
+                });
+                if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
+                    // Caso a pesquisa retorne algo e esse retorno tenha um tamanho maior que 0 retorna um erro de que já existe um cadastro com esse email
+                    return res
+                        .status(400)
+                        .json({ erro: "Esse email já está cadastrado" });
+                }
+                // enviar a imagem do multer para o cosmic
+                const image = await uploadImagemCosmic(req);
+
+                // salvar no banco de dados
+                const usuarioASerSalvo = { // Cria uma const com o que é esperado de um usuario e isso foi definido no Model
+                    nome: usuario.nome,
+                    email: usuario.email,
+                    senha: md5(usuario.senha), // Utilização do md5 para que o dado seja guardado criptografado, caso haja algum vazamento a senha ao menos estará com uma criptografia
+                    avatar: image?.media?.url, // Caso exista uma imagem ela será mandada para o avatar do usuário
+                };
+
+                await UsuarioModel.create(usuarioASerSalvo); // O método create cria o objeto definido em usuarioASerSalvo na Coleção
+                return res.status(200).json({ msg: "Usuário cadastrado com sucesso" });
+            } catch (e) {
+                console.log(e);
+                return res.status(500).json({ erro: "erro ao cadastrar o usuário" });
+            }
         }
-        if (!usuario.email || usuario.email.length < 5 || !usuario.email.includes('@') || !usuario.email.includes('.')) {
-            return res.status(400).json({ erro: 'Email invalido. Tente novamente.' }); //Vai retornar se colocarem alguma informação de login e senha errada
-        }
-        if (!usuario.senha || usuario.senha.length < 4) {
-            return res.status(400).json({ erro: 'Senha invalida. Tente novamente ou altere a mesma.' }); //Vai retornar se colocarem alguma informação de login e senha errada
-        }
-
-        //Validacao se ja existe usuario com o mesmo email
-        const usuariosComMesmoEmail = await UsuarioModel.find({ email: usuario.email });
-        if (usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0) {
-            return res.status(400).json({ erro: 'Ja existe uma conta com o email informado' });
-        }
-
-        //salvar no banco de dados
-        const usuarioASerSalvo = {
-            nome: usuario.nome,
-            email: usuario.email,
-            senha: md5(usuario.senha) //md5 é utilizado para criptografar informações. Nesse caso é a senha
-        }
-        await UsuarioModel.create(usuarioASerSalvo);
-        return res.status(200).json({ msg: 'Usuario criado com sucesso' });
-    }
-    return res.status(405).json({ erro: 'O metodo informado não é valido' }); //Vai retornar o método 405 (solicitação de algo que não está permitido) se usarem outro método diferente de POST
-}
-
-export default conectarMongoDB(endpointCadastro);
+    );
+export const config = { // Configurando para que o NEXT não mande uma request com um JSON, por estarmos enviando uma foto é necessário que seja um FormData
+    api: {
+        bodyParser: false,
+    },
+}; 
+export default conectarMongoDB(handler); // Export o handler criado passando os middlewares necessários antes
--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,12 +1,67 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
-import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
-import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
-import { UsuarioModel } from '../../models/UsuarioModel';
+import type { NextApiRequest, NextApiResponse } from "next";
+import nc from "next-connect";  // Request, Response e Handler padrão do Next
+import { validarTokenJWT } from "@/middlewares/validarTokenJWT"; // Importando o middleware de validação do token JWT criado
+import { conectarMongoDB } from "@/middlewares/conectarMongoDB"; // Importando o middleware de conexão com DB que foi criado
+//import { politicaCORS } from "@/middlewares/politicaCORS"; // Importando o middleware de CORS que criamos
+import type { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg"; // Importando o tipo de resposta padrão que criamos
+import { UsuarioModel } from "@/models/UsuarioModel"; // Importando o model do Usuário
+import { upload, uploadImagemCosmic } from "@/service/uploadImagemCosmic"; // Importando as funções que criamos para utilização do Cosmic e Mutler
 
-const usuarioEndpoint = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = nc() // Utilizando o nc do Next Connect que serve para "gerenciar" as requisições e resposta HTTP
+    .use(upload.single('file')) // utiliza o Multer para fazer upload de um único arquivo
+    .put( // Utiliza o método PUT do NC para atualizar os dados do usuário
+        async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
+            try {
+                const { userId } = req?.query;  // Usando um destructor cria uma constante userId com a propriedade userId que vem no query da request se a request existe
+                const usuario = await UsuarioModel.findById(userId); // Busca no Banco de Dados o usuário pelo id
 
-    return res.status(200).json('Usuario autenticado com sucesso');
+                if (!usuario) { // Caso a busca não retorne um usuário retorna um erro de usuário não encontrado
+                    return res.status(400).json({ erro: 'Usuário não encontrado' })
+                };
+
+                const { nome } = req.body; // Usando um destructor cria uma constante nome com a propriedade nome que vem no query
+                if (nome && nome.length > 2) { // Caso o nome exista no corpo da request e tenha mais de 2 caracteres nós atribuimos o valor a propriedade nome do usuário
+                    usuario.nome = nome;
+                };
+
+                const { file } = req;  // Usando um destructor cria uma constante file com a propriedade file que vem no request
+                if (file && file.originalname) { // Verifica se há algum file e se há um originalname no file
+                    const image = await uploadImagemCosmic(req); // Utiliza o upload do cosmic para colocar a imagem no bucket
+                    if (image && image.media && image.media.url) {  // Caso alguma imagem seja carregada e exista a propriedade media e a propriedade url dentro de media iremos atribuir a url ao avatar do usuário
+                        usuario.avatar = image.media.url;
+                    }
+
+                }
+
+                await UsuarioModel.findByIdAndUpdate({ _id: usuario._id }, usuario); // Atualizaremos o usuário no DB com o que foi passado para a mudança
+
+                return res.status(200).json({ msg: 'Usuário alterado com sucesso' });
+
+            } catch (e) {
+                console.log(e);
+                return res.status(400).json({ erro: 'Não foi possível atualizar o usuário' + e });
+            }
+
+        }).get( // Utiliza o método GET do NC para obter dados do usuário
+            async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+                try {
+                    const { userId } = req?.query;  // Usando um destructor cria uma constante userID com a propriedade userID que vem no query da request se a request existe
+                    const usuario = await UsuarioModel.findById(userId);  // Busca no Banco de Dados o usuário pelo id
+
+                    usuario.senha = null; // Definimos usuario.senha como null para a senha do usuário não ficar trafegando, pois quando fazemos a pesquisa pelo ID do usuário a DB retorna todos os dados existentes na coleção referente aquele usuário
+                    return res.status(200).json(usuario); // Retorna com sucesso o usuário
+
+                } catch (e) {
+                    console.log(e);
+                    return res.status(400).json({ erro: 'Não foi possível obter dados do usuário' })
+                }
+            });
+
+export const config = { // Passa uma configuração para o NEXT não transformar a resposta em JSON e enviar a request como FormData devido a estarmos passando um arquivo
+    api: {
+        bodyParser: false
+    }
 }
 
-export default validarTokenJWT(usuarioEndpoint);
+
+export default validarTokenJWT(conectarMongoDB(handler));
